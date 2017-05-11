188a8d88bd9af10b5ffb314c93ac0abbcd52d63a
Trenchevski <Trenchevski@users.noreply.github.com>
2017-05-11 22:25:12 +0200

    Add files via upload

---
+++ python/ex11.py
@@ -0,0 +1,11 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+print "What is this internship about?",
+internship = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy, and you are %r intern." % (
+    age, height, weight, internship)
